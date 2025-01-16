68f909a83c6700f8d7212108b55f79ca727ae87c
samuelrrangel <samuel.rrangel529@gmail.com>
2025-01-16 08:31:27 -0300

    wip: adicionar novos controladores do back-end

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  password: { type: String, required: false },
+  profile: { type: String, required: false },
   age: { type: Number, required: true },
+  role: { type: String, required: true },
 });
 
 module.exports = mongoose.model('User', userSchema);
