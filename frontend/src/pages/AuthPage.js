532c45e1cd28f4d2c558b5b90657a6a4a8ffed0f
samuelrrangel <samuel.rrangel529@gmail.com>
2025-01-19 02:33:20 -0300

    fix(Auth): aplicar elementos e estrutura básica para página de autenticação

--- frontend/src/pages/AuthPage.js
+++ frontend/src/pages/AuthPage.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const EventsPage = () => (
+const AuthPage = () => (
   <main>
-      <h1>Log de eventos</h1>
-      <EventLog/>
+    <div class={"outerAuth"}>
+      <div class={"innerAuth"}>
+        <div class={"authNav"}>
+          <Link to="">Login</Link>
+          <Link >
+          </Link>
+        </div>
+        <div class={"authForm"}>
+        </div>
+      </div>
+    </div>
   </main>
 );
 
-export default EventsPage;
+export default AuthPage;
